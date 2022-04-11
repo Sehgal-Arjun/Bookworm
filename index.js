9cf82109a96965cbbdc359becc37a3d7228d7b79
Arjun-Sehgal <arjun_sehgal@asl.org>
2022-04-11 19:22:16 +0100

    slimming down book choices
    No Magazines or books without similar books attribute listed in the books variable

---
+++ index.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+var books = [];
+/*
+$(document).ready(function () {
+    main();
+});
+function main(){
+    //Making books (parsing .JSON)////////////////////////////
+    var data = JSON.parse(fs.readFileSync('books.json'));   //
+    for (var i = 0; i < data.books.length; i++){            //
+        books.push(data.books[i]);}                         //                                                         
+    //////////////////////////////////////////////////////////
+    console.log(books.length);
+    console.log(books[0].author_name);
+}
+*/
+//Making books (parsing .JSON)////////////////////////////
+var data = JSON.parse(fs.readFileSync('books.json'));
+for (var i = 0; i < data.books.length; i++){
+    books.push(data.books[i]);}
+for (var i = 0; i < books.length; i++){
+    if (books[i].author_name == "IF Magazine"){
+        books.splice(i,1);
+    }
+    else if (books[i].author_name == "Galaxy Magazine"){
+        books.splice(i,1);
+    }
+    else if (books[i].similar_books !== undefined){ 
+        if (books[i].similar_books.length == 0){
+            books.splice(i,1);
+        }
+    }
+}
+//////////////////////////////////////////////////////////
+
+console.log(books.length);
--- test.js
+++ test.js
@@ -1,52 +1,5 @@
-const results = [];
-let books = [];
-
-class Book{
-
-    constructor(t, a, c, th, d, py, ar, p, nr) {
-        this.title = t;
-        this.authors = a;
-        this.categories = c;
-        this.thumbnail = th;
-        this.description = d;
-        this.publishedYear = py;
-        this.averageRating = ar;
-        this.pages = p;
-        this.numRatings = nr;
-    }
-
-    getName(){
-        return this.title;
-    }
-    getAuthors(){
-        return this.authors;
-    }
-    getCategories(){
-        return this.categories;
-    }
-    getThumbnail(){
-        return this.thumbnail;
-    }
-    getDescription(){
-        return this.description;
-    }
-    getYear(){
-        return this.publishedYear;
-    }
-    getRating(){
-        return this.averageRating;
-    }
-    getPages(){
-        return this.pages;
-    }
-    getNumRatings(){
-        return this.numRatings;
-    }
-
-}
-
-window.onload = function(){
-    document.getElementById('RecText').innerHTML = '<img src="http://books.google.com/books/content?id=KQZCPgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"/>';
-}
-
-var bigString = ``
+fetch('books.json')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
