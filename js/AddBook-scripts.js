d9e4a052fb857081acea3fe7b5d6a7d7c6b8dc20
Arjun-Sehgal <arjun_sehgal@asl.org>
2022-05-19 11:19:18 +0100

    Complete ?
    1) created initial favicon - will likely change it in the future
    2) made it visible to the user what similar books there are to their search (eg, typing Hary Potter will show you Harry Potter books, instead of just saying that Hary Potter doesn't exist)
    3) Added functionality to show you if your add/remove/clear worked or not

--- js/AddBook-scripts.js
+++ js/AddBook-scripts.js
@@ -1,6 +1,6 @@
 jQuery.easing.jswing=jQuery.easing.swing;jQuery.extend(jQuery.easing,{def:"easeOutQuad",swing:function(e,f,a,h,g){return jQuery.easing[jQuery.easing.def](e,f,a,h,g)},easeInQuad:function(e,f,a,h,g){return h*(f/=g)*f+a},easeOutQuad:function(e,f,a,h,g){return -h*(f/=g)*(f-2)+a},easeInOutQuad:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f+a}return -h/2*((--f)*(f-2)-1)+a},easeInCubic:function(e,f,a,h,g){return h*(f/=g)*f*f+a},easeOutCubic:function(e,f,a,h,g){return h*((f=f/g-1)*f*f+1)+a},easeInOutCubic:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f+a}return h/2*((f-=2)*f*f+2)+a},easeInQuart:function(e,f,a,h,g){return h*(f/=g)*f*f*f+a},easeOutQuart:function(e,f,a,h,g){return -h*((f=f/g-1)*f*f*f-1)+a},easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f+a}return -h/2*((f-=2)*f*f*f-2)+a},easeInQuint:function(e,f,a,h,g){return h*(f/=g)*f*f*f*f+a},easeOutQuint:function(e,f,a,h,g){return h*((f=f/g-1)*f*f*f*f+1)+a},easeInOutQuint:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f*f+a}return h/2*((f-=2)*f*f*f*f+2)+a},easeInSine:function(e,f,a,h,g){return -h*Math.cos(f/g*(Math.PI/2))+h+a},easeOutSine:function(e,f,a,h,g){return h*Math.sin(f/g*(Math.PI/2))+a},easeInOutSine:function(e,f,a,h,g){return -h/2*(Math.cos(Math.PI*f/g)-1)+a},easeInExpo:function(e,f,a,h,g){return(f==0)?a:h*Math.pow(2,10*(f/g-1))+a},easeOutExpo:function(e,f,a,h,g){return(f==g)?a+h:h*(-Math.pow(2,-10*f/g)+1)+a},easeInOutExpo:function(e,f,a,h,g){if(f==0){return a}if(f==g){return a+h}if((f/=g/2)<1){return h/2*Math.pow(2,10*(f-1))+a}return h/2*(-Math.pow(2,-10*--f)+2)+a},easeInCirc:function(e,f,a,h,g){return -h*(Math.sqrt(1-(f/=g)*f)-1)+a},easeOutCirc:function(e,f,a,h,g){return h*Math.sqrt(1-(f=f/g-1)*f)+a},easeInOutCirc:function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a},easeInElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return -(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e},easeOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return g*Math.pow(2,-10*h)*Math.sin((h*k-i)*(2*Math.PI)/j)+l+e},easeInOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k/2)==2){return e+l}if(!j){j=k*(0.3*1.5)}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}if(h<1){return -0.5*(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e}return g*Math.pow(2,-10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j)*0.5+l+e},easeInBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*(f/=h)*f*((g+1)*f-g)+a},easeOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*((f=f/h-1)*f*((g+1)*f+g)+1)+a},easeInOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}if((f/=h/2)<1){return i/2*(f*f*(((g*=(1.525))+1)*f-g))+a}return i/2*((f-=2)*f*(((g*=(1.525))+1)*f+g)+2)+a},easeInBounce:function(e,f,a,h,g){return h-jQuery.easing.easeOutBounce(e,g-f,0,h,g)+a},easeOutBounce:function(e,f,a,h,g){if((f/=g)<(1/2.75)){return h*(7.5625*f*f)+a}else{if(f<(2/2.75)){return h*(7.5625*(f-=(1.5/2.75))*f+0.75)+a}else{if(f<(2.5/2.75)){return h*(7.5625*(f-=(2.25/2.75))*f+0.9375)+a}else{return h*(7.5625*(f-=(2.625/2.75))*f+0.984375)+a}}}},easeInOutBounce:function(e,f,a,h,g){if(f<g/2){return jQuery.easing.easeInBounce(e,f*2,0,h,g)*0.5+a}return jQuery.easing.easeOutBounce(e,f*2-g,0,h,g)*0.5+h*0.5+a}});
 
-if (localStorage.getItem('cleared') == "s1"){
+if (localStorage.getItem('cleared') == "1"){
     document.getElementById('response').innerHTML = "Cleared.";
     localStorage.setItem('cleared', '0');
 }
@@ -18,6 +18,15 @@ if ((userBooksObj.length !== 0)){
 }
 
 function add(){
+    document.getElementById('similar').innerHTML = "";
+    document.getElementById('thumb').src = "";
+    document.getElementById('nameauthor').innerHTML = "";
+    document.getElementById('thumb1').src = "";
+    document.getElementById('nameauthor1').innerHTML = "";
+    document.getElementById('thumb2').src = "";
+    document.getElementById('nameauthor2').innerHTML = "";
+    document.getElementById('similar').innerHTML = "";
+
 
     userBooks = JSON.parse(localStorage.getItem('userbooks') || '[]');
     var name = document.getElementById("input").value;  
@@ -53,8 +62,72 @@ function add(){
         }
     }
     if (inList == false){
+        let tempbooks = books;
+        let removelist = [];
         console.log("adding: " + !adding);
         document.getElementById('response').innerHTML = "That's not a valid book.";
+        let count = 0;
+        for (let i = 0; i < tempbooks.length; i++){
+            let addscore = stringSimilarity.compareTwoStrings(tempbooks[i].title.toLowerCase(),name.toLowerCase()); // above 0.4/0.5 is pretty decent similarity after testing
+            if (addscore >= 0.4){
+                tempbooks[i].isbn10 = addscore;
+                count = count + 1;
+            }
+            else{
+                removelist.push(tempbooks[i]);
+            }
+        }
+        if (count == 0){
+            document.getElementById('similar').innerHTML = "There were no matches for that book.";
+        }
+        else{
+            for (let i = 0; i < removelist.length; i++){
+                let index = tempbooks.indexOf(removelist[i]);
+                tempbooks.splice(index, 1);
+            }
+            for (let i = 0; i < tempbooks.length; i++){
+                tempbooks[i].isbn13 = tempbooks[i].isbn10 + tempbooks[i].average_rating;
+            }
+            tempbooks.sort(function(a, b){
+                return a.isbn13-b.isbn13;
+            })
+            tempbooks.reverse();
+            console.log(tempbooks);
+            console.log("count: " + count);
+
+
+            if (count == 1){
+                document.getElementById('similar').innerHTML = "There was one similar match.";
+                document.getElementById('thumb').src = tempbooks[0].thumbnail;
+                document.getElementById('nameauthor').innerHTML = "Name: " + tempbooks[0].title + ", Author(s): " + tempbooks[0].authors.replace(/;/g, ", ");
+            }
+            if (count == 2){
+            document.getElementById('similar').innerHTML = "There were two similar matches for that book.";
+            document.getElementById('thumb').src = tempbooks[0].thumbnail;
+            document.getElementById('nameauthor').innerHTML = "Name: " + tempbooks[0].title + ", Author(s): " + tempbooks[0].authors.replace(/;/g, ", ");
+            document.getElementById('thumb1').src = tempbooks[1].thumbnail;
+            document.getElementById('nameauthor1').innerHTML = "Name: " + tempbooks[1].title + ", Author(s): " + tempbooks[1].authors.replace(/;/g, ", ");
+            }
+            if (count == 3){
+            document.getElementById('similar').innerHTML = "There were three similar matches for that book.";
+            document.getElementById('thumb').src = tempbooks[0].thumbnail;
+            document.getElementById('nameauthor').innerHTML = "Name: " + tempbooks[0].title + ", Author(s): " + tempbooks[0].authors.replace(/;/g, ", ");
+            document.getElementById('thumb1').src = tempbooks[1].thumbnail;
+            document.getElementById('nameauthor1').innerHTML = "Name: " + tempbooks[1].title + ", Author(s): " + tempbooks[1].authors.replace(/;/g, ", ");
+            document.getElementById('thumb2').src = tempbooks[2].thumbnail;
+            document.getElementById('nameauthor2').innerHTML = "Name: " + tempbooks[2].title + ", Author(s): " + tempbooks[2].authors.replace(/;/g, ", ");
+            }
+            if (count > 3){
+            document.getElementById('similar').innerHTML = "There were three similar matches for that book.";
+            document.getElementById('thumb').src = tempbooks[0].thumbnail;
+            document.getElementById('nameauthor').innerHTML = "Name: " + tempbooks[0].title + ", Author(s): " + tempbooks[0].authors.replace(/;/g, ", ");
+            document.getElementById('thumb1').src = tempbooks[1].thumbnail;
+            document.getElementById('nameauthor1').innerHTML = "Name: " + tempbooks[1].title + ", Author(s): " + tempbooks[1].authors.replace(/;/g, ", ");
+            document.getElementById('thumb2').src = tempbooks[2].thumbnail;
+            document.getElementById('nameauthor2').innerHTML = "Name: " + tempbooks[2].title + ", Author(s): " + tempbooks[2].authors.replace(/;/g, ", ");
+            document.getElementById('similar').innerHTML = "There were " + count + " similar matches for that book. Here's the top three.";
+            }
+        }
     }
 
     console.log(userBooks);
